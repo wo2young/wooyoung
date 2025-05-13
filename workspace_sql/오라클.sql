5d1c6fc37285e05447dcd3663357d1eae83af459
김우영 <ywk5974@gmail.com>
2025-05-13 13:59:52 +0900

    2차 중간

--- workspace_sql/오라클.sql
+++ workspace_sql/오라클.sql
@@ -1,10 +1,10 @@
--- 한줄 주석
+-- 한줄 주석              주석 자주 사용
 /* 범위 주석 */
 /* 여러줄
    주석
-*/
+*/            --1일차
 
--- select문 조회
+-- select문 조회      기본 중요
 select * from emp;
 select * from dept;
 select * from salgrade;
@@ -16,7 +16,7 @@ select EMPNO, ENAME, DEPTNO
 select ename, sal
 from emp;
 
---distinct문 중복데이터 삭제
+--distinct문 중복데이터 삭제  별로 안중요
 select deptmo
 from emp;
 
@@ -30,7 +30,7 @@ from emp;
 select all job, deptno
 from emp;
 
---별칭 as는 생략가능
+--별칭 as는 생략가능             나름 사용
 select ename, sal, sal*12+comm, comm
 from emp;
 
@@ -40,4 +40,83 @@ from emp;
 --"" 사용시 뛰어쓰기 가능 ""는 별칭에만 사용
 select ename, sal, sal*12+comm as "s  s", comm 추가수당
 from emp;
+            --2일차
+--order by절(정렬) 생략시 asc(오름차순) / desc(내림차순)   자주사용
+select *
+from emp
+order by sal;
+
+select *
+from emp
+order by sal desc;
+
+select *
+from emp
+order by deptno asc, sal desc;
+
+select *
+from emp
+order by ename desc;
+
+select *
+from emp
+order by deptno, sal desc, empno desc;
+
+--되새김 문제
+select distinct job
+from emp;
+
+select empno employee_no, ename employee_name, mgr manager,
+sal salary, comm commisson, deptno department_no  
+from emp
+order by  department_no desc, employee_no asc;
+
+--where절(조건)
+select *
+from emp
+WHERE deptno = 30;
+
+--where 조건에 and, or 사용
+select *
+from emp
+where deptno = 30
+and job = 'SALESMAN';
+
+select *
+from emp
+where deptno = 30
+or job = 'CLERK';
+
+--연산자 종류 산술, 비교, 논리 부정, in, between a and b, like, is null, 집합 등
+select *
+from emp
+where sal*12 = 36000;
+
+select *
+from emp
+where sal >= 3000;
+
+select *
+from emp
+where ename >= 'F';
+
+select *
+from emp
+where ename <= 'FORZ';
+-- != 과 <>, ^= 은 같은 결과값을 낸다.
+select *
+from emp
+where sal != 3000;
+
+select *
+from emp
+where sal <> 3000;
+
+select *
+from emp
+where sal ^= 3000;
+
+select *
+from emp
+where not sal = 3000;
 
