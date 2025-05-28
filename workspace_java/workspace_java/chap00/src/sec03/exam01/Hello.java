a33e8d2bcbf43520ed3e0d1917d31eea9dcdcb36
wo2young <ywk5974@gmail.com>
2025-05-28 16:46:57 +0900

    Add files via upload

---
+++ workspace_java/workspace_java/chap00/src/sec03/exam01/Hello.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+
+// 한줄 주석
+/* 범위 주석 */
+
+public class Hello {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			System.out.println("Hello World");
+
+   }
+
+}
