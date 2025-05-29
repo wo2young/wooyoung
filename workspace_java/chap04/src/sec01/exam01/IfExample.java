a87fba6334059f70d1ab850a6f110256aa10356d
김우영 <ywk5974@gmail.com>
2025-05-30 08:59:46 +0900

    ddd

--- workspace_java/chap04/src/sec01/exam01/IfExample.java
+++ workspace_java/chap04/src/sec01/exam01/IfExample.java
@@ -47,7 +47,27 @@ public class IfExample {
 		System.out.println(random1);
 		
 		
-		
+	 int dice = (int) Math.random();
+	 dice = Math.random() * 46
+			 System.out.println(dice);
+	 
+	 //  사용가능 : char, byte, short, int, string
+	 // 사용불가 : boolean, long, float, double
+	 
+	 int month = 1;
+	 if (month = 1) \\ (month = 2) \\ (month = 12) {
+		 System.out.println("겨울");
+	 } else if (month = 3) \\ (month = 4) \\ (month = 5) {
+		 System.out.println("봄");
+	 } else if (month = 6) \\ (month = 7) \\ (month = 8) {
+		 System.out.println("여름");
+	 } else (month = 9) \\ (month = 10) \\ (month = 11) {
+		 System.out.println("가을");
+	 }
+	 
+	 
+	 
+	 
 	} 
 
 }
