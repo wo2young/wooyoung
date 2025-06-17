3b61fe05c83f3982098bce1682872040b17bf335
BOOK-L0DKHG7KP0\ywk59 <ywk5974@gmail.com>
2025-06-17 17:59:33 +0900

    dd

---
+++ workspace_java/chap01/src/sec01/exam01/Exam.java
@@ -0,0 +1,32 @@
+package sec01.exam01;
+
+public class Exam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i =0;
+		int [] s = new int [6];
+		// 10~5까지 출력
+		//		1        2 5 8   4 7 10
+		for(int x = 10; x > 4; x--) {
+			//    3 6 9 
+			for(int j=0; j<1;j++) {
+				s[i] = x;
+				
+				System.out.println("s[" + i + "] : " + s[i]);
+				i++;
+			}
+				
+		}
+		
+//		for while 반복문
+//		if switch 조건문
+		 i = 1;
+		if( i == 1 ) {
+			System.out.println("팀장 김동현");
+		}
+
+	}
+
+}
