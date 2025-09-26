df9cf35a194708596ab26a051adfd0c8214b83bc
김우영 <ywk5974@gmail.com>
2025-09-26 17:40:42 +0900

    c 드가자

---
+++ woelspace_c#/Console_Hello/Program.cs
@@ -0,0 +1,19 @@
+﻿class program
+{
+    public static void Main(String[] args)
+    {
+        Console.Write("??????");
+        Console.WriteLine("Hello, World!");
+        int[] arr = { 1, 2, 3, 4, 5 };
+        foreach(int i in arr)
+        {
+            Console.WriteLine(i + ",");
+        }
+    }
+
+    public static void test()
+    {
+        Console.WriteLine("test");
+    }
+}
+
---
+++ woelspace_c#/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello, World!";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
